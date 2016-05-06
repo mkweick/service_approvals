2686183da0086e60d831939c127d7545ad977f3d
Matt Weick <mkweick@gmail.com>
2016-05-06 12:33:47 -0400

    add default image for no picture and visual improvements

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -1,10 +1,58 @@
 require 'odbc'
 
 class ApprovalsController < ApplicationController
-  before_action :valid_order?, only: [:index]
 
   def index
-    
+    if params[:repairkey].nil?
+      redirect_to 'http://www.divalsafety.com'
+    else
+      repair_key = params[:repairkey].strip
+      as400_83m = ODBC.connect('approvals_m')
+
+      sql_find_order = "SELECT a.tlorno, a.tlstatus03, a.tlstatus04, a.tlticket,
+                                c.cmcsno, c.cmcsnm, b.trmodel, b.trserial, d.tt$tot
+                        FROM toolr_log AS a
+                        JOIN toolrpctl AS b ON b.trticket = a.tlticket
+                        JOIN aplus83fds.cusms AS c ON c.cmcsno = b.trcsno
+                        JOIN tooltot AS d ON d.ttpror = a.tlpror
+                        WHERE tllinkkey = '#{repair_key}'"
+          
+      stmt_find_order = as400_83m.run(sql_find_order)
+      order = stmt_find_order.fetch_all
+      
+      as400_83m.commit
+      as400_83m.disconnect
+
+      if order.nil?
+        flash.alert = "This is not a valid repair ticket."
+      else
+        order = order.first.map(&:strip)
+        order_num = order[0]
+        @declined = order[1] == '3'
+        @accepted = order[2] == '4'
+        @ticket_num = order[3]
+        @cust_num = order[4]
+        @cust_name = order[5]
+        @model_num = order[6]
+        @serial_num = order[7]
+        @order_total = order[8]
+        @image_path = "no_image.png"
+        @image_alt = "No image for Ticket #{@ticket_num}"
+
+        uri = URI.parse("https://repair.divalsafety.com/toolrep/" + @ticket_num + "-B.jpg")
+        response_code = Net::HTTP.get_response(uri).code
+        if response_code == "200"
+          @image_path = "https://repair.divalsafety.com/toolrep/" + @ticket_num + "-B.jpg"
+          @image_alt = "Image for Ticket #{@ticket_num}"
+        end
+
+        if @accepted
+          flash.now.notice = "Ticket #{@ticket_num} has already been approved"
+        elsif @declined
+          flash.now.alert = "Ticket #{@ticket_num} has already been declined"
+        end
+      end
+    end
   end
 
   def approve
@@ -57,51 +105,5 @@ class ApprovalsController < ApplicationController
     
     as400_83m.commit
     as400_83m.disconnect
-  end
-
-  private
-
-  def valid_order?
-    if params[:repairkey].nil?
-      redirect_to 'http://www.divalsafety.com'
-    else
-      repair_key = params[:repairkey].strip
-      as400_83m = ODBC.connect('approvals_m')
-
-      sql_find_order = "SELECT a.tlorno, a.tlstatus03, a.tlstatus04, a.tlticket,
-                                c.cmcsno, c.cmcsnm, b.trmodel, b.trserial, d.tt$tot
-                        FROM toolr_log AS a
-                        JOIN toolrpctl AS b ON b.trticket = a.tlticket
-                        JOIN aplus83fds.cusms AS c ON c.cmcsno = b.trcsno
-                        JOIN tooltot AS d ON d.ttpror = a.tlpror
-                        WHERE tllinkkey = '#{repair_key}'"
-          
-      stmt_find_order = as400_83m.run(sql_find_order)
-      order = stmt_find_order.fetch_all
-      
-      as400_83m.commit
-      as400_83m.disconnect
-
-      if order.nil?
-        flash.alert = "This is not a valid repair ticket."
-      else
-        order = order.first.map(&:strip)
-        order_num = order[0]
-        @declined = order[1] == '3'
-        @accepted = order[2] == '4'
-        @ticket_num = order[3]
-        @cust_num = order[4]
-        @cust_name = order[5]
-        @model_num = order[6]
-        @serial_num = order[7]
-        @order_total = order[8]
-
-        if @accepted
-          flash.notice = "Ticket #{@ticket_num} has already been approved"
-        elsif @declined
-          flash.alert = "Ticket #{@ticket_num} has already been declined"
-        end
-      end
-    end
   end
 end
